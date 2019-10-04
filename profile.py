cdb7b3f89c4b134abda78e80c01b5f157abb9b79
John Sangwon Suh <johnsuh94@gmail.com>
2019-10-03 21:53:07 -0400

    update profile

--- profile.py
+++ profile.py
@@ -4,22 +4,15 @@ Instructions:
 Wait for the profile instance to start, then click on the node in the topology and choose the `shell` menu item.
 """
 
-# Import the Portal object.
 import geni.portal as portal
-# Import the ProtoGENI library.
-import geni.rspec.pg as pg
-
-# Create a portal context.
-pc = portal.Context()
+import geni.rspec.pg as rspec
 
 # Create a Request object to start building the RSpec.
-request = pc.makeRequestRSpec()
+request = portal.context.makeRequestRSpec()
 
-# Add a raw PC to the request.
 node = request.RawPC("node")
 
-# Install and execute a script that is contained in the repository.
-node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU18-64-STD"
 
-# Print the RSpec to the enclosing page.
-pc.printRequestRSpec(request)
+
+portal.context.printRequestRSpec()
