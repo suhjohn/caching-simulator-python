096114fcadc9f2d53d6a230d5deb4a97aabe3dfb
John Sangwon Suh <johnsuh94@gmail.com>
2020-04-01 09:56:45 -0400

    add false positive test for bloom filters

---
+++ bloom_filter_fp_experiment.py
@@ -0,0 +1,29 @@
+import argparse
+
+import os
+
+import settings
+from filters import BloomFilter, SetFilter, SetFilterArgs
+from traces import initialize_iterator, DEFAULT_TRACE_TYPE
+
+
+def run_test(trace_dir, trace_file, filter_args):
+    trace_file_path = f"{trace_dir}/{trace_file}"
+    bloom_filter = BloomFilter(filter_args)
+    set_filter = SetFilter(SetFilterArgs())
+    trace_iterator = initialize_iterator(DEFAULT_TRACE_TYPE, trace_file_path)
+
+    fp_count = 0
+    for request in trace_iterator:
+        fp_count += int(bloom_filter.should_filter(request) != set_filter.should_filter(request))
+
+    print(fp_count / trace_iterator.total_count)
+    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('traceFile')
+    args = parser.parse_args()
+
+    trace_dir = os.environ.get("TRACE_DIRECTORY", settings.TRACE_DIRECTORY)
+    filter_args = settings.FILTER_ARGS
